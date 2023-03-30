ce1f75a5c274035c7623a1b0a33e8c4c8f6da1fb
Sierra737 <126513183+Sierra737@users.noreply.github.com>
2023-03-30 11:09:25 +0300

    Add files via upload

--- script1.js
+++ script1.js
@@ -12,67 +12,60 @@ function getStaffNames() {
               {alert('empty record!');
                 break;
               }
-        }
+    }
     
-      return arrNames;
+    return arrNames;
 }
+
 let staffNames=getStaffNames(); 
 
 // створюємо команду
 
 function createTeam() {
 
-    console.log(`Names of the staff are:`);
-   console.log(staffNames);
-   console.log(`Positions names are:`)
-   console.log(arrPosit);
- 
      let team ={};
 
      for(let i=0; i<staffNames.length; i++){
 
-     team[`number${i+1}`]={};
-     team[`number${i+1}`].name=staffNames[i];
-     team[`number${i+1}`].position=arrPosit[i];
+        team[i]={};
+        team[i].name=staffNames[i];
+        team[i].position=arrPosit[i];
 
      }
-     console.log(`My staff object is:`);
-     console.log(team);
      
      return team;
 }
     
-createTeam();
+
     
   //встановлюємо зарплатню
-  function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+function getRandomInt(min, max) {
+       return Math.floor(Math.random() * (max - min + 1)) + min;
 }
   
 
- function setSalary() {
-    let a= {};
+function setSalary() {
+    
     let lowered=[];
+    let myTeam=createTeam();
+
  for(let i=0; i<staffNames.length; i++){
      lowered[i]=arrPosit[i].toLowerCase();
-      
-     a[`number${i+1}`]={};
-     a[`number${i+1}`].name=staffNames[i];
-     a[`number${i+1}`].position=arrPosit[i];
+         
 
-  if (lowered[i].indexOf('junior')===0)
- {a[`number${i+1}`].salary=getRandomInt(500,1000);}
- else if (lowered[i].indexOf('middle')===0)
- {a[`number${i+1}`].salary=getRandomInt(1500,2000);}
- else if (lowered[i].indexOf('senior')===0)
- {a[`number${i+1}`].salary=getRandomInt(2500,3000);}
- else {a[`number${i+1}`].salary=getRandomInt(4000,4500);}
- }
+    if (lowered[i].indexOf('junior')===0)
+       {myTeam[i].salary=getRandomInt(500,1000);}
+         else if (lowered[i].indexOf('middle')===0)
+                 {myTeam[i].salary=getRandomInt(1500,2000);}
+                 else if (lowered[i].indexOf('senior')===0)
+                         {myTeam[i].salary=getRandomInt(2500,3000);}
+                         else {myTeam[i].salary=getRandomInt(4000,4500);}
+  }
  
- console.log(`My staff object with salaries is:`)
- console.log(a);
+ console.log(`My whole staff object is:`)
+ console.log(myTeam);
 
- return(a);
+ return(myTeam);
  
 }
 
@@ -80,31 +73,31 @@ createTeam();
 
 function tellAboutYou() {
  
-    let team3=setSalary();
+    let team=setSalary();
 
  
-console.log(`Telling about each worker:`)
-  for(let i=0; i<staffNames.length; i++){
-  team3[`number${i+1}`].tellAboutYourSelf=function(){console.log(`My name is ${team3[`number${i+1}`].name} and I am-${team3[`number${i+1}`].position}.I earn ${team3[`number${i+1}`].salary}\$`)};
-  team3[`number${i+1}`].tellAboutYourSelf();
- }
- 
-return(team3);
+    console.log(`Telling about each worker:`)
+    for(let i=0; i<staffNames.length; i++){
+      team[i].tellAboutYourSelf=function(){console.log(`My name is ${team[i].name} and I am-${team[i].position}.I earn ${team[i].salary}\$`)};
+      team[i].tellAboutYourSelf();
+    }
+    
+    return(team);
 }
 
 
 
 function showAll(){
 
-let team1=tellAboutYou();
+let team=tellAboutYou();
 
-
-team1.showTeam = function() {
+team.showTeam = function() {
     console.log(`All information about staff:`)
-  for(let i=0; i<staffNames.length; i++){
-    console.log(`${team1[`number${i+1}`].name}-${team1[`number${i+1}`].position}.Salary ${team1[`number${i+1}`].salary}\$`)}
-   }
-   team1.showTeam();  
+    for(let i=0; i<staffNames.length; i++){
+        console.log(`${team[i].name}-${team[i].position}.Salary ${team[i].salary}\$`)}
+    }
+
+team.showTeam();  
   
 }
 
